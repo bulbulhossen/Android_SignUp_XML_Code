81ee0cf5527a7a6d7e54b2fe005bb87a3719cd77
bulbul.hossen <info247.bulbul@gmail.com>
2015-12-09 03:10:34 +0600

    Update Code

--- app/src/main/java/com/example/bulbulhossen/hoverimage/MainActivity.java
+++ app/src/main/java/com/example/bulbulhossen/hoverimage/MainActivity.java
@@ -1,13 +1,29 @@
 package com.example.bulbulhossen.hoverimage;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.pay);
+
+
+        textView = (TextView) findViewById(R.id.login);
+        textView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, login.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
---
+++ app/src/main/java/com/example/bulbulhossen/hoverimage/login.java
@@ -0,0 +1,16 @@
+package com.example.bulbulhossen.hoverimage;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by bulbul.hossen on 12/9/2015.
+ */
+public class login extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
